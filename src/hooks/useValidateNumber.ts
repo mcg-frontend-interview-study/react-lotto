df0a45bd3a3f859cc679e8e3a9d399a2e55e3339
bmj0053 <bmj05378@gmail.com>
2024-12-26 15:56:47 +0900

    fix: useValidateNumber 확인 타입 오류 해결

--- src/hooks/useValidateNumber.ts
+++ src/hooks/useValidateNumber.ts
@@ -4,8 +4,7 @@ const useValidateNumber = () => {
   const [isValid, setIsValid] = useState(true);
 
   const validateNumber = (value: string) => {
-    const numberValue = Number(value);
-    if (isNaN(numberValue)) {
+    if (Number.isNaN(value)) {
       alert('숫자만 입력해 주세요!');
       setIsValid(false);
       return;
