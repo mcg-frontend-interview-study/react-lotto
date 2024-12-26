fe803f70470cc1deb47ac9511d646df8a41c01c6
bmj0053 <bmj05378@gmail.com>
2024-12-26 15:43:05 +0900

    feat: Context에 inputAmountValue, 당첨 번호, 보너스 번호 추가

--- src/contexts/LottoContext.tsx
+++ src/contexts/LottoContext.tsx
@@ -2,20 +2,42 @@ import { createContext, useState } from 'react';
 import { RandomNumbersType } from '../types/randomNumbers';
 
 interface LottoContextProps {
+  inputAmountValue: string;
+  setInputAmountValue: (amount: string) => void;
   lottoNumbers: RandomNumbersType;
   lottoCount: number;
   setLottoNumbers: (numbers: RandomNumbersType) => void;
   setLottoCount: (count: number) => void;
+  winningNumbers: string[];
+  bonusNumber: string;
+  setWinningNumbers: (numbers: string[]) => void;
+  setBonusNumber: (number: string) => void;
 }
 
 const LottoContext = createContext<LottoContextProps | undefined>(undefined);
 
 export const LottoProvider = ({ children }: React.PropsWithChildren) => {
+  const [inputAmountValue, setInputAmountValue] = useState('');
   const [lottoNumbers, setLottoNumbers] = useState<RandomNumbersType>({});
   const [lottoCount, setLottoCount] = useState<number>(0);
+  const [winningNumbers, setWinningNumbers] = useState(Array(6).fill(''));
+  const [bonusNumber, setBonusNumber] = useState('');
 
   return (
-    <LottoContext.Provider value={{ lottoNumbers, lottoCount, setLottoNumbers, setLottoCount }}>
+    <LottoContext.Provider
+      value={{
+        inputAmountValue,
+        setInputAmountValue,
+        lottoNumbers,
+        lottoCount,
+        setLottoNumbers,
+        setLottoCount,
+        winningNumbers,
+        setWinningNumbers,
+        bonusNumber,
+        setBonusNumber,
+      }}
+    >
       {children}
     </LottoContext.Provider>
   );
