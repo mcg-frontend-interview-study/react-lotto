e2601d89319d5a86405a70c8ec5d795b2703d138
김진호 <rlawlsgh1227@gmail.com>
2024-12-26 13:40:19 +0900

    feat: Button 컴포넌트 생성

---
+++ src/components/Button/Button.style.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  padding: 6px 14px;
+
+  background-color: ${({ theme }) => theme.color.primary};
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  white-space: nowrap;
+
+  ${({ theme }) => theme.typography.caption};
+  color: ${({ theme }) => theme.color.grayScale1};
+
+  &:active {
+    background-color: ${({ theme }) => theme.color.primaryVariants};
+  }
+`;
