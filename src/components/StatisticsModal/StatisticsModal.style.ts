7574e88f13a8c297a1f0a413e8856f158467e3a5
김진호 <rlawlsgh1227@gmail.com>
2024-12-26 16:07:55 +0900

    feat: 당첨 통계 모달 퍼블리싱 완료

--- src/components/StatisticsModal/StatisticsModal.style.ts
+++ src/components/StatisticsModal/StatisticsModal.style.ts
@@ -12,6 +12,7 @@ export const DimmedLayer = styled.div`
 
 export const Modal = styled.section`
   display: flex;
+  flex-direction: column;
   position: fixed;
 
   left: 50%;
@@ -22,6 +23,9 @@ export const Modal = styled.section`
 
   width: 350px;
   height: 500px;
+
+  padding: 32px 16px;
+  gap: 32px;
 
   border-radius: 4px;
 `;
@@ -36,4 +40,54 @@ export const CloseButton = styled.button`
   border: none;
 
   cursor: pointer;
+`;
+
+export const Title = styled.h2`
+  ${({ theme }) => theme.typography.title};
+  text-align: center;
+`;
+
+export const Table = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const HeaderRow = styled.div`
+  display: flex;
+  width: 100%;
+  height: 40px;
+
+  padding: 8px 0;
+
+  border-top: 1px solid ${({ theme }) => theme.color.grayScale5opacity12};
+  border-bottom: 1px solid ${({ theme }) => theme.color.grayScale5opacity12};
+
+  ${({ theme }) => theme.typography.body};
+  font-weight: 600;
+`;
+
+export const Row = styled.div`
+  display: flex;
+  width: 100%;
+  height: 40px;
+
+  padding: 8px 0;
+
+  border-bottom: 1px solid ${({ theme }) => theme.color.grayScale5opacity12};
+
+  ${({ theme }) => theme.typography.body};
+  font-weight: 400;
+`;
+
+export const Column = styled.div<{ $flex: number }>`
+  flex: ${({ $flex }) => $flex};
+  text-align: center;
+  white-space: nowrap;
+`;
+
+export const Return = styled.p`
+  ${({ theme }) => theme.typography.body};
+  font-weight: 600;
+
+  text-align: center;
 `;
--- src/components/StatisticsModal/StatisticsModal.tsx
+++ src/components/StatisticsModal/StatisticsModal.tsx
@@ -2,6 +2,7 @@ import * as Styled from './StatisticsModal.style';
 import { createPortal } from 'react-dom';
 import { useRef } from 'react';
 import { ClickOutsideDetector } from '../ClickOutsideDetector/ClickOutSideDetector';
+import { Button } from '../Button/Button';
 
 type StatisticsModalProps = {
   close: () => void;
@@ -15,6 +16,41 @@ export const StatisticsModal = ({ close }: StatisticsModalProps) => {
       <ClickOutsideDetector targetRef={modalRef} onClickOutside={close}>
         <Styled.Modal ref={modalRef}>
           <Styled.CloseButton onClick={close}>X</Styled.CloseButton>
+          <Styled.Title>🏆 당첨 통계 🏆</Styled.Title>
+          <Styled.Table>
+            <Styled.HeaderRow>
+              <Styled.Column $flex={3}>일치 갯수</Styled.Column>
+              <Styled.Column $flex={4}>당첨금</Styled.Column>
+              <Styled.Column $flex={3}>당첨 갯수</Styled.Column>
+            </Styled.HeaderRow>
+            <Styled.Row>
+              <Styled.Column $flex={3}>3개</Styled.Column>
+              <Styled.Column $flex={4}>5,000</Styled.Column>
+              <Styled.Column $flex={3}>n개</Styled.Column>
+            </Styled.Row>
+            <Styled.Row>
+              <Styled.Column $flex={3}>4개</Styled.Column>
+              <Styled.Column $flex={4}>50,000</Styled.Column>
+              <Styled.Column $flex={3}>n개</Styled.Column>
+            </Styled.Row>
+            <Styled.Row>
+              <Styled.Column $flex={3}>5개</Styled.Column>
+              <Styled.Column $flex={4}>1,500,000</Styled.Column>
+              <Styled.Column $flex={3}>n개</Styled.Column>
+            </Styled.Row>
+            <Styled.Row>
+              <Styled.Column $flex={3}>5개+보너스볼</Styled.Column>
+              <Styled.Column $flex={4}>30,000,000</Styled.Column>
+              <Styled.Column $flex={3}>n개</Styled.Column>
+            </Styled.Row>
+            <Styled.Row>
+              <Styled.Column $flex={3}>6개</Styled.Column>
+              <Styled.Column $flex={4}>2,000,000,000</Styled.Column>
+              <Styled.Column $flex={3}>n개</Styled.Column>
+            </Styled.Row>
+          </Styled.Table>
+          <Styled.Return>당신의 총 수익률은 %입니다.</Styled.Return>
+          <Button>다시 시작하기</Button>
         </Styled.Modal>
       </ClickOutsideDetector>
     </Styled.DimmedLayer>,
